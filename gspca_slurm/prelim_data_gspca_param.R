5cb8d91c9197b32f0695bc4b73c621efd18d0db9
Michael Anderson <52722296+and02709@users.noreply.github.com>
2023-11-08 21:07:41 -0600

    initial upload

---
+++ gspca_slurm/prelim_data_gspca_param.R
@@ -0,0 +1,77 @@
+args <- commandArgs()
+# define arguments
+setwd <- args[6]
+xfile <- args[7]
+yfile <- args[8]
+npc <- as.numeric(args[9])
+nfolds <- as.numeric(args[10])
+groupdf <- args[11]
+gparams <- args[12]
+kernel <- args[13]
+niter <- as.numeric(args[14])
+trace <- as.numeric(args[15])
+balance <- as.numeric(args[16])
+cat("setwd: ",setwd,"\n")
+cat("xfile: ", xfile,"\n")
+cat("yfile: ",yfile,"\n")
+cat("npc: ",npc,"\n")
+cat("nfolds: ",nfolds,"\n")
+cat("groups: ",groupdf,"\n")
+cat("gparams: ",gparams,"\n")
+cat("kernel: ",kernel,"\n")
+cat("niter: ",niter,"\n")
+cat("trace: ",trace,"\n")
+cat("balance: ",balance,"\n")
+## check parameters
+cat("read in arguments \n")
+cat("about to read in data \n")
+X <- read.table(xfile)
+Y <- read.table(yfile)
+X <- as.matrix(X)
+Y <- as.matrix(Y)
+groups <- read.table(file=groupdf, header=F) |> as.matrix()
+nonzero.groups <- read.table(file=gparams, header=F) |> as.matrix()
+cat("read in data \n")
+n <- nrow(X)
+p <- ncol(X)
+cat("label data \n")
+colnames(X) <-paste0("x",c(1:p))
+colnames(Y) <- "y"
+
+unique.groups <- unique(groups)
+num.group <- length(unique.groups)
+if(sum((nonzero.groups-floor(nonzero.groups))==0) != length(nonzero.groups)) stop("Must specify integer groups")
+if(min(nonzero.groups) < 1) stop("Must specify minimum number of nonzero groups as at least 1")
+if(max(nonzero.groups) > num.group) stop("Cannot have more nonzero groups than total number of groups")
+
+rownames(X) <- c(1:n)
+rownames(Y) <- c(1:n)
+ind.names <- rownames(X)
+
+if(kernel!="linear" && kernel!="delta") stop("Please select a valid kernel")
+df <- data.frame(Y,X)
+num.nz.gr <- length(nonzero.groups)
+if(kernel=="delta" && part.balance){
+  df.partition <- groupdata2::fold(data=df,k=nfolds,cat_col = "y")
+} else{
+  df.partition <- groupdata2::fold(data=df,k=nfolds)
+}
+cat("partitioned data \n")
+fold.arg <- c(1:nfolds)
+param.grid <- expand.grid(fold.arg,nonzero.groups)
+colnames(param.grid) <- c("fold.arg","gr.arg")
+cat("set up argument grid \n")
+fpath <- paste(setwd,"temp",sep="")
+dfpath <- paste(fpath,"/df.txt",sep="")
+parampath <- paste(fpath,"/param.txt",sep="")
+gpath <- paste(fpath,"/garg.txt",sep="")
+glpath <- paste(fpath,"/gl.txt",sep="")
+cat("set up paths \n")
+write.table(df.partition,file=dfpath,row.names = T,col.names = T,quote=F)
+cat("save partitioned data \n")
+write.table(param.grid,file=parampath,row.names = F,col.names = T,quote=F)
+cat("save parameter grid \n")
+write.table(nonzero.groups,file=gpath,quote=F)
+cat("save group param argument vector \n")
+write.table(groups,file=glpath,quote=F)
+cat("save group index vector \n")
